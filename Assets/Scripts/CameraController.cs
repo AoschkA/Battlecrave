501488c8a297561e384112056ec7b3f8b09ab068
Alexander V. Pedersen <alexander12350@hotmail.com>
2016-11-01 15:17:05 +0100

    CameraController

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject gamePlane;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+	    Vector3 gamePlanePosition = gamePlane.transform.position;
+        Vector3 gamePlaneCenter = 
+
 	}
 	
 	// Update is called once per frame
