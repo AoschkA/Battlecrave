79f388ceb2cc5e18188e048b975c8e9e3065c4a2
AoschkA <jonaspraem@gmail.com>
2016-11-02 20:35:35 +0100

    Added basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,51 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-    private Animator animator;
+    public float speed = 6f;
 
-	// Use this for initialization
-	void Start () {
+    private Animator animator;
+    Vector3 movement;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+	void Awake() {
+        floorMask = LayerMask.GetMask("Floor");
         animator = GetComponent<Animator>();
-	}
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-        float moveVertical = Input.GetAxis("Vertical");
-        
-        if (moveVertical != 0) {
-            animator.SetBool("isMoving", true);
-        } else {
-            animator.SetBool("isMoving", false);
+	void FixedUpdate() {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turn();
+        Animate(h, v);
+    }
+	void Move(float h, float v) {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turn() {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
         }
-	}
+    }
+
+    void Animate(float h, float v) {
+        bool walking = h != 0f || v != 0f;
+        animator.SetBool("isMoving", walking);
+
+    }
 }
