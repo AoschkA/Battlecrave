362debb231e686d77a8bfae422e31779ec8887a8
Blackflunk <Alexander12350@hotmail.com>
2016-11-03 12:35:28 +0100

    Camera controller done

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,21 +5,53 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject gamePlane;
+	public GameObject player;
+	//private GameObject camera;
+	private Vector3 gamePlaneCenter;
+	private Vector3 directionFromCenter;
+
 
 	// Use this for initialization
 	void Start ()
 	{
-	    Vector3 gamePlanePosition = gamePlane.transform.position;
-	    Vector3 gamePlaneCenter;
-	    gamePlaneCenter.x = gamePlanePosition.x/2;
-	    gamePlaneCenter.y = gamePlanePosition.y/2;
-	    gamePlaneCenter.z = gamePlanePosition.z/2;
+		//gamePlane = GetComponent<GameObject> ();
+		//camera = GetComponent<GameObject> ();
+	   	
+		findPlaneCenter ();
+
+
 
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		findCameraPosition ();
+		Debug.Log (directionFromCenter);
+
+	}
+
+	void findPlaneCenter(){
+		Vector3 gamePlanePosition = gamePlane.transform.position;
+
+		gamePlaneCenter.x = gamePlanePosition.x/2;
+		gamePlaneCenter.y = gamePlanePosition.y/2;
+		gamePlaneCenter.z = gamePlanePosition.z/2;
+	}
+
+	void findCameraPosition(){
+		Vector3 playerPosition = player.transform.position;
+		directionFromCenter = (playerPosition - gamePlaneCenter).normalized;
+		Vector3 cameraNewPosition = new Vector3 (directionFromCenter.x * 13, 5, directionFromCenter.z * 13);
+
+			/*
+		transform.position.x = directionFromCenter.x * 13;
+		transform.position.z = directionFromCenter.z * 13;
+		transform.position.y = 5;
+*/
+		transform.position = cameraNewPosition;
+		transform.LookAt (gamePlaneCenter);
+
+
 	}
 }
