031880a773313541864dc0272f8b94401c3fe55e
Blackflunk <Alexander12350@hotmail.com>
2016-11-04 16:39:25 +0100

    Kamera opdateret, virker bedre, kamera controller sat på test

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,9 +6,11 @@ public class CameraController : MonoBehaviour
 
     public GameObject gamePlane;
 	public GameObject player;
+	public int cameraDistance = 13;
 	//private GameObject camera;
 	private Vector3 gamePlaneCenter;
 	private Vector3 directionFromCenter;
+
 
 
 	// Use this for initialization
@@ -40,16 +42,25 @@ public class CameraController : MonoBehaviour
 	}
 
 	void findCameraPosition(){
+		// Find player position
 		Vector3 playerPosition = player.transform.position;
+
+		// Find direction from center to player
 		directionFromCenter = (playerPosition - gamePlaneCenter).normalized;
-		Vector3 cameraNewPosition = new Vector3 (directionFromCenter.x * 13, 5, directionFromCenter.z * 13);
+
+		//New position, 13 indicates the distance, it is static, 5 indicates the height.
+		Vector3 cameraNewPosition = new Vector3 (directionFromCenter.x * cameraDistance, 5, directionFromCenter.z * cameraDistance);
+
+		// Lerping the position for better camera movement.
+		Vector3 cameraPositionLerped = Vector3.Lerp(transform.position, cameraNewPosition, Time.deltaTime * 2);
 
 			/*
 		transform.position.x = directionFromCenter.x * 13;
 		transform.position.z = directionFromCenter.z * 13;
 		transform.position.y = 5;
 */
-		transform.position = cameraNewPosition;
+		//transform.position = cameraNewPosition;
+		transform.position = cameraPositionLerped;
 		transform.LookAt (gamePlaneCenter);
 
 
