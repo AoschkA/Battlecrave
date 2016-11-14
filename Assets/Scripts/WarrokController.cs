eef3e0d5af50e36d166d2b382a6fc46484548f95
Jonas Praem <Jonas Praem>
2016-11-14 16:48:46 +0100

    Added new boss with animations

---
+++ Assets/Scripts/WarrokController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Timers;
+
+public class WarrokController : MonoBehaviour {
+    private Animator anim;
+    private long trigger = 0;
+    public Animation punch;
+
+    void Awake() {
+        anim = GetComponent<Animator>();
+
+    }
+	void Update () {
+        trigger++;
+        if (trigger % 400 == 0) {
+            Animate();
+        }
+
+	}
+
+    void Animate() {
+        Debug.Log("trigger");
+        anim.SetTrigger("punch");
+    }
+
+    void ArcaneBolt() {
+        Debug.Log("punch");
+    }
+}
