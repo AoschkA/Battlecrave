8cb133ed2e2bd63cef07fae841f4fcc2f35e49bd
praem <jonaspraem@gmail.com>
2016-11-24 20:45:58 +0100

    added abilities

---
+++ Assets/Scripts/Abilities/Ability.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Ability : MonoBehaviour {
+    public double cooldown;
+
+    public abstract void UseAbility();
+
+    public abstract void Animate();
+}
---
+++ Assets/Scripts/Abilities/Immunity.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Immunity : Ability {
+    public int duration;
+
+    GameObject player;
+
+    void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public override void UseAbility() {
+        throw new NotImplementedException();
+    }
+
+    public override void Animate() {
+        throw new NotImplementedException();
+    }
+}
