1de3420f95ef28d1e730bb1897217a93b3ee17de
Blackflunk <Alexander12350@hotmail.com>
2016-12-12 15:00:58 +0100

    Scene added for test, round surface added, boss floating away?

---
+++ Assets/Scripts/BoxMoveTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxMoveTest : MonoBehaviour {
+	
+	public float thrust;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("w"))
+			rb.AddForce(Vector3.forward * thrust);
+		if (Input.GetKeyDown("a"))
+			rb.AddForce(Vector3.left * thrust);
+		if (Input.GetKeyDown("d"))
+			rb.AddForce(Vector3.right * thrust);
+		if (Input.GetKeyDown("s"))
+			rb.AddForce(Vector3.back * thrust);
+	}
+}
