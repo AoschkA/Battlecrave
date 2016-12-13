b006926de7c5b88ee534878dc0653130b3843a50
AoschkA <jonaspraem@gmail.com>
2016-12-13 22:40:34 +0100

    randomized boss abilites

--- Assets/Scripts/Enemy/WarrokController.cs
+++ Assets/Scripts/Enemy/WarrokController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Timers;
+using System;
 
 public class WarrokController : MonoBehaviour {
     public GameObject arcaneBoltPrefab;
@@ -10,17 +11,19 @@ public class WarrokController : MonoBehaviour {
     private long trigger = 0;
     private GameObject player;
     private GameObject arcaneBolt;
+    private System.Random rnd;
 
     void Awake() {
         anim = GetComponent<Animator>();
         player = GameObject.FindGameObjectWithTag("Player");
         arcaneBolt = GameObject.FindGameObjectWithTag("Arcane Bolt");
-
+        rnd = new System.Random();
     }
 	void Update () {
         trigger++;
-        if (trigger % 400 == 0) {
-            Animate();
+        if (trigger % 200 == 0) {
+            int attackNumber = rnd.Next(1, 5);
+            Animate(attackNumber);
         }
 
         // Rotate boss
@@ -28,9 +31,17 @@ public class WarrokController : MonoBehaviour {
 
     }
 
-    void Animate() {
+    void Animate(int attackNumber) {
         Debug.Log("trigger");
-        anim.SetTrigger("punch");
+        if (attackNumber == 1) {
+            anim.SetTrigger("punch");
+        }
+        if (attackNumber == 2) {
+            anim.SetTrigger("swipe");
+        }
+        if (attackNumber == 3) {
+            anim.SetTrigger("flex");
+        }
     }
 
     void ArcaneBolt() {
