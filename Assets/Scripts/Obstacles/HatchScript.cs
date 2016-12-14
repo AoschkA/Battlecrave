ae8aab79528558506131e1e7f5ec9fe8685b345e
Blackflunk <Alexander12350@hotmail.com>
2016-12-14 18:48:42 +0100

    Hatches work by pressing button, to be on attack

---
+++ Assets/Scripts/Obstacles/HatchScript.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatchScript : MonoBehaviour {
+	private bool open; 
+	private bool openIsUnset; 
+	public GameObject turnAroundPoint;
+	private int hatchID;
+
+	// Use this for initialization
+	void Start () {
+		openIsUnset = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F)) {
+			openIsUnset = false;
+			if (open)
+				open = false;
+			else if (!open)
+				open = true;
+
+		}
+		if (open && !openIsUnset) {
+			
+			if (turnAroundPoint.transform.position.z-2 > transform.position.z && turnAroundPoint.transform.position.x == transform.position.x)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.left, 90 * Time.deltaTime);
+			
+			if (turnAroundPoint.transform.position.z+2 < transform.position.z && turnAroundPoint.transform.position.x == transform.position.x)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.right, 90 * Time.deltaTime);
+			
+			if (turnAroundPoint.transform.position.x-2 > transform.position.x && turnAroundPoint.transform.position.z == transform.position.z)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.forward, 90 * Time.deltaTime);
+			
+			if (turnAroundPoint.transform.position.x+2 < transform.position.x && turnAroundPoint.transform.position.z == transform.position.z)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.back, 90 * Time.deltaTime);	
+		}
+	
+		if (open == false && !openIsUnset) {
+			//Top
+			if (turnAroundPoint.transform.position.z > transform.position.z && turnAroundPoint.transform.position.x == transform.position.x && transform.position.y<=0)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.right, 90 * Time.deltaTime);
+			//Bottom
+			if (turnAroundPoint.transform.position.z < transform.position.z && turnAroundPoint.transform.position.x == transform.position.x && transform.position.y<=0)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.left, 90 * Time.deltaTime);
+			//Right
+			if (turnAroundPoint.transform.position.x > transform.position.x && turnAroundPoint.transform.position.z == transform.position.z && transform.position.y<=0)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.back, 90 * Time.deltaTime);
+			//Left
+			if (turnAroundPoint.transform.position.x < transform.position.x && turnAroundPoint.transform.position.z == transform.position.z && transform.position.y<=0)
+				transform.RotateAround (turnAroundPoint.transform.position, Vector3.forward, 90 * Time.deltaTime);	
+		}
+	}
+}
