4bb0fb70452ee976fb9fadf1c4666f27ba8bd0a1
Blackflunk <Alexander12350@hotmail.com>
2016-12-15 13:54:26 +0100

    Hatches works properly according to colliders

---
+++ Assets/Scripts/Obstacles/HatchCollisionScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HatchCollisionScript : MonoBehaviour {
+	private bool isPlayerCollidingHatch;
+	public GameObject hatch;
+	private HatchScript hs;
+	private GameObject floor;
+	private MeshCollider floorCollider;
+
+
+	// Use this for initialization
+	void Start () {
+		hs = (HatchScript)hatch.GetComponent (typeof(HatchScript));
+		floor = GameObject.FindGameObjectWithTag ("Floor");
+		floorCollider = floor.GetComponent<MeshCollider> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider playerCollision){
+		Debug.Log ("Test");
+		if (playerCollision.gameObject.tag == "Player") {
+			isPlayerCollidingHatch = true;
+			Debug.Log ("Entering collision with player (Hatch)");
+			if (hs.GetIsHatchOpen())
+				floorCollider.enabled = false;
+			else
+				floorCollider.enabled = true;
+
+		}
+	}
+
+	void OnTriggerExit(Collider playerCollision){
+		if (playerCollision.gameObject.tag == "Player") {
+			isPlayerCollidingHatch = false;
+			Debug.Log ("Exiting collision with player (Hatch)");
+			floorCollider.enabled = true;
+		}
+	}
+
+	public bool GetIsPlayerCollidingHatch(){
+		return isPlayerCollidingHatch;
+	}
+}
--- Assets/Scripts/Obstacles/HatchScript.cs
+++ Assets/Scripts/Obstacles/HatchScript.cs
@@ -6,39 +6,45 @@ public class HatchScript : MonoBehaviour {
 	private bool openIsUnset; 
 	public GameObject turnAroundPoint;
 	private int hatchID;
+	private GameObject player;
+	private bool isPlayerCollidingHatch;
 
 	// Use this for initialization
 	void Start () {
 		openIsUnset = true;
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.F)) {
 			openIsUnset = false;
-			if (open)
+			if (open) {
 				open = false;
-			else if (!open)
+			} else if (!open) {
 				open = true;
+			}
 
 		}
 		if (open && !openIsUnset) {
-			
+			// -2 and +2 due to the fact that I don't want hatches to open to 90 degrees, this allows difference Δxz to find either top,left,right or bottom hatch.
+			//Top
 			if (turnAroundPoint.transform.position.z-2 > transform.position.z && turnAroundPoint.transform.position.x == transform.position.x)
 				transform.RotateAround (turnAroundPoint.transform.position, Vector3.left, 90 * Time.deltaTime);
-			
+			//Bottom
 			if (turnAroundPoint.transform.position.z+2 < transform.position.z && turnAroundPoint.transform.position.x == transform.position.x)
 				transform.RotateAround (turnAroundPoint.transform.position, Vector3.right, 90 * Time.deltaTime);
-			
+			//Right
 			if (turnAroundPoint.transform.position.x-2 > transform.position.x && turnAroundPoint.transform.position.z == transform.position.z)
 				transform.RotateAround (turnAroundPoint.transform.position, Vector3.forward, 90 * Time.deltaTime);
-			
+			//Left
 			if (turnAroundPoint.transform.position.x+2 < transform.position.x && turnAroundPoint.transform.position.z == transform.position.z)
 				transform.RotateAround (turnAroundPoint.transform.position, Vector3.back, 90 * Time.deltaTime);	
 		}
 	
 		if (open == false && !openIsUnset) {
-			//Top
+			//Top, first and second part of the if sentance describes if it is a hatch, third part secures that it is only rotated to match the floor.
 			if (turnAroundPoint.transform.position.z > transform.position.z && turnAroundPoint.transform.position.x == transform.position.x && transform.position.y<=0)
 				transform.RotateAround (turnAroundPoint.transform.position, Vector3.right, 90 * Time.deltaTime);
 			//Bottom
@@ -52,4 +58,9 @@ public class HatchScript : MonoBehaviour {
 				transform.RotateAround (turnAroundPoint.transform.position, Vector3.forward, 90 * Time.deltaTime);	
 		}
 	}
+
+	public bool GetIsHatchOpen(){
+		return open;
+	}
+		
 }
