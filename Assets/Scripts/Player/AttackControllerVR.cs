7388c9f644bb8fe929d2e1c59f4ea27495febd2f
Blackflunk <Alexander12350@hotmail.com>
2017-01-02 15:56:07 +0100

    VR added

---
+++ Assets/Scripts/Player/AttackControllerVR.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AttackControllerVR : MonoBehaviour
+{
+    // Bullet emitter to shoot from.
+    public GameObject BulletEmitter;
+
+    // Actual bullet.
+    public GameObject Bullet;
+
+	GameObject camera;
+
+    // Speed.
+    public float BulletForce;
+
+    //ShootCooldown
+    private bool ShootCooldown = false;
+    private float CooldownTimeleft = 0.5f;
+
+	// Use this for initialization
+	public void Awake () {
+		camera = this.gameObject.transform.GetChild(2).gameObject;
+	}
+
+	// Update is called once per frame
+	public void Update () {
+        if(ShootCooldown)
+            StartShootCooldown();
+
+		float shootSwipeX = Input.GetAxis ("Mouse X");
+
+		if (shootSwipeX < -0.4)
+		{
+	    
+			if (!ShootCooldown)
+			{
+			
+	            GameObject TempBullet;
+
+	            TempBullet =
+					Instantiate(Bullet, BulletEmitter.transform.position, camera.transform.rotation) as GameObject;
+
+
+
+	            // Fail correction
+	            //TempBullet.transform.Rotate(Vector3.left * 90);
+
+	            // RigidBody
+	            Rigidbody TempBulletRigidBody;
+	            TempBulletRigidBody = TempBullet.GetComponent<Rigidbody>();
+
+	            //Ignore collision between bullet and player.
+	            //Physics.IgnoreCollision(TempBullet.GetComponent<Collider>(), this.gameObject.GetComponent<Collider>());
+
+	            // Add force to bullet
+				TempBulletRigidBody.AddForce((camera.transform.forward*BulletForce));
+
+                
+
+	            // Destroy
+	            Destroy(TempBullet, 10.0f);
+
+               
+
+                ShootCooldown = true;
+
+
+                }
+	    }
+
+	}
+
+    public void StartShootCooldown ()
+    {
+        CooldownTimeleft -= Time.deltaTime;
+
+        if (CooldownTimeleft < 0)
+        {
+            ShootCooldown = false;
+            CooldownTimeleft = 0.5f;
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/Player/MovementVR.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementVR : MonoBehaviour {
+	public float movementSpeed = 4f;
+	public float turnSmoothing = 15f; // A smoothing value for turning the player.
+
+	private Animator animator;
+	Vector3 movement;
+	Rigidbody playerRigidbody;
+	int floorMask;
+	float camRayLength = 100f;
+	GameObject camera;
+	GameObject playerModel;
+
+
+	// Use this for initialization
+	void Start () {
+		floorMask = LayerMask.GetMask("Floor");
+		animator = GetComponent<Animator>();
+		playerRigidbody = GetComponent<Rigidbody>();
+		camera = this.gameObject.transform.GetChild(2).gameObject;
+		playerModel = this.gameObject.transform.GetChild(0).gameObject;
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+		Turn ();
+	}
+
+
+
+	void Move() {
+		// Grab this frame's input from the GearVR pad.
+		float moveX = Input.GetAxis("Mouse X");
+		float moveY = Input.GetAxis("Mouse Y");
+		Debug.Log ("X: " + moveX);
+		Debug.Log ("Y: " + moveY);
+
+
+		// Define local vectors based on where I'm facing
+		Vector3 forward = transform.TransformDirection(Vector3.forward);
+		Vector3 backward = transform.TransformDirection(Vector3.back);
+		Vector3 left = transform.TransformDirection(Vector3.left);
+		Vector3 right = transform.TransformDirection(Vector3.right);
+
+		Vector3 movement = new Vector3(0f,0f,0f);
+
+		if (Input.GetMouseButton(0) && moveX == 0 && moveY == 0) {
+			movement += camera.transform.forward * Time.deltaTime * movementSpeed;
+		} /*else if (moveY < -0.1 && Input.GetMouseButtonDown(0)) {
+			movement -= transform.forward * Time.deltaTime * movementSpeed;
+		} if (moveX > 0.1 && Input.GetMouseButtonDown(0)) {
+			movement -= transform.right * Time.deltaTime * movementSpeed;
+		} else if (moveX < -0.1 && Input.GetMouseButtonDown(0)) {
+			movement += transform.right * Time.deltaTime * movementSpeed;
+		}*/
+
+		playerRigidbody.MovePosition(transform.position + movement);
+
+	}
+
+	void Turn(){
+		Ray camRay = Camera.main.ScreenPointToRay(camera.transform.forward);
+
+		RaycastHit floorHit;
+
+		if (Physics.Raycast (camRay, out floorHit, camRayLength, floorMask)) {
+			Vector3 playerToMouse = floorHit.point - transform.position;
+			playerToMouse.y = 0f;
+
+			Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+
+			playerModel.transform.rotation = newRotation;
+
+			//playerModelRB.MoveRotation (newRotation);
+
+		
+
+
+		}
+	}
+
+}
