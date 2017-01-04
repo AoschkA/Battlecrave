8e776b014a8b8756f258f23ef0105bc87515622b
Blackflunk <Alexander12350@hotmail.com>
2017-01-04 01:56:22 +0100

    added recticle

---
+++ Assets/Scripts/Recticle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Recticle : MonoBehaviour {
+	public Camera cameraFacing;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = cameraFacing.transform.position + cameraFacing.transform.rotation * Vector3.forward * 2;
+		transform.LookAt (cameraFacing.transform.position);
+		transform.Rotate (0.0f, 180.0f, 0.0f);
+	}
+}
